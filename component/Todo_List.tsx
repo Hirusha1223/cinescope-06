88990e78cabe3ff048e76078ce0a48a9c3d80f86
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2025-12-27 08:46:10 +0530

    refactor:add a photo to new componenet and fix errors in page.tsx page

---
+++ component/Todo_List.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr&apos;s Todos</h1>
+      <Image
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo"
+        width={200} 
+        height={300}
+
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
