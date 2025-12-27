533838e0be76de3cde408a3a5131d5de9c550df5
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2025-12-27 10:22:07 +0530

    chore: setup shadcn button primitive and use

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import TodoList from "@/component/Todo_List";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRightIcon } from "lucide-react";
 
 
 
@@ -7,6 +9,21 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <TodoList/>
+        <h1 className="text-lg font-bold capitalize">Need More information&apos;s</h1>
+        <div className="flex gap-2 w-full flex-wrap">
+          <Button>Click Me</Button>
+          <Button variant={"outline"}>Click Me</Button>
+          <Button size="icon-sm" aria-label="Submit" variant="outline">
+            <ArrowUpRightIcon />
+          </Button>
+          <Button variant={"secondary"}>Click Me</Button>
+          <Button variant={"ghost"}>Click Me</Button>
+          <Button variant={"link"}>Click Me</Button>
+          <Button variant={"destructive"}>Click Me</Button>
+          <Button className="bg-amber-200">Click Me</Button>
+
+
+        </div>
       </main>
     </div>
   );
