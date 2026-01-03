9ce26e5fde1ec9f7ae84a4362f6c064581e79a98
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-03 16:34:41 +0530

    added the header content and footer section in dashboad page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,25 @@
 export default function dashboardPage(){
     return (
-  <div className="flex min-h-screen items-center bg-zinc-50 justify-center font-sans dark:bg-black">
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 p">
-      <h1 className="text-2xl font-bold">Dashboard Page</h1>
+        <div className="flex flex-col min-h-screen">
+      {/* {Header Section} */}
+
+    <header className="flex-1 flex items-center justify-center bg-blue-200">
+      <span>I&apos;m the header section</span>
+    </header>
+
+    {/* {Main Section} */}
+
+    <main className="flex-10 flex items-center justify-center bg-yellow-200">
+      <h1>This&apos;s Dashboard Page</h1>
     </main>
-  </div>
+
+    {/* {Footer Section} */}
+
+    <footer className="flex-1 flex items-center justify-center bg-red-600">
+      <span>I&apos;m the Footer section</span>
+    </footer>
+
+
+    </div>
 );
 }
