61a7cae91960d82cd849a3f48a7c984dde7892c7
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-03 08:49:00 +0530

    chore:make the page.tsx in dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function dashboardPage(){
+    return (
+  <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 p">
+      <h1 className="text-2xl font-bold">Dashboard Page</h1>
+    </main>
+  </div>
+);
+}
