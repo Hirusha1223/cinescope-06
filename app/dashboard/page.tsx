078c2b5236e8a3b08a74f11f7043d7fc482de33d
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-03 10:44:25 +0530

    chore: did some changes in page.tsx add background color

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function dashboardPage(){
     return (
-  <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+  <div className="flex min-h-screen items-center bg-zinc-50 justify-center font-sans dark:bg-black">
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 p">
       <h1 className="text-2xl font-bold">Dashboard Page</h1>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red-500`}
       >
         {children}
       </body>
