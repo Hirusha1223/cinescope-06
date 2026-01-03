ef43722c6d5e914e3c429aa89b524a64f764b2a3
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-03 16:41:01 +0530

    feat: add about section and make the page.tsx in

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+<div className="flex flex-col min-h-screen">
+      {/* {Header Section} */}
+
+    <header className="flex-1 flex items-center justify-center bg-blue-200">
+      <span>I&apos;m the header section</span>
+    </header>
+
+    {/* {Main Section} */}
+
+    <main className="flex-10 flex items-center justify-center bg-yellow-200">
+      <span>I&apos;m the header section</span>
+    </main>
+
+    {/* {Footer Section} */}
+
+    <footer className="flex-1 flex items-center justify-center bg-red-600">
+      <span>I&apos;m the header section</span>
+    </footer>
+
+
+    </div>
