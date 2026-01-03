2023682c9dfea1f9dba8c07e22febbf261248da5
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-03 16:09:58 +0530

    chore:make the simple header content and footer section in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,31 @@
-import TodoList from "@/component/Todo_List";
-import { Button } from "@/components/ui/button";
-import { ArrowUpRightIcon } from "lucide-react";
+// import TodoList from "@/component/Todo_List";
+// import { Button } from "@/components/ui/button";
+// import { ArrowUpRightIcon } from "lucide-react";
 
 
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-amber-400 dark:bg-black sm:items-start">
-        <TodoList/>
-        <h1 className="text-lg font-bold capitalize">Need More information&apos;s</h1>
-        <div className="flex gap-2 w-full flex-wrap">
-          <Button>Click Me</Button>
-          <Button variant={"outline"}>Click Me</Button>
-          <Button size="icon-sm" aria-label="Submit" variant="outline">
-            <ArrowUpRightIcon />
-          </Button>
-          <Button variant={"secondary"}>Click Me</Button>
-          <Button variant={"ghost"}>Click Me</Button>
-          <Button variant={"link"}>Click Me</Button>
-          <Button variant={"destructive"}>Click Me</Button>
-          <Button className="bg-amber-200">Click Me</Button>
+    <div className="flex flex-col min-h-screen">
+      {/* {Header Section} */}
+
+    <header className="flex-1 flex items-center justify-center bg-blue-200">
+      <span>I&apos;m the header section</span>
+    </header>
+
+    {/* {Main Section} */}
+
+    <main className="flex-10 flex items-center justify-center bg-yellow-200">
+      <span>I&apos;m the header section</span>
+    </main>
+
+    {/* {Footer Section} */}
+
+    <footer className="flex-1 flex items-center justify-center bg-red-600">
+      <span>I&apos;m the header section</span>
+    </footer>
 
 
-        </div>
-      </main>
     </div>
   );
 }
