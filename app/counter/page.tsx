64452d84fb5035268383d93f2423163d48f7c652
Hirusha1223 <ar112483@fhss.sjp.ac.lk>
2026-01-17 11:40:29 +0530

    feat: made the new componenet in counter sub route call counter-class Modularizing the UI

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,20 +1,11 @@
-"use client";
+import Counterfunction from "./counet-func.jsx";
+import CounterClass from "./counter-class.jsx";
 
-import { useState } from "react";
-
-export default function Counter() {
-  const [count, setCounter] = useState(0);
-
+export default function Counterpage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
-      <p className="text-2xl font-medium">Count {count}</p>
-
-      <button
-        onClick={() => setCounter(count + 1)}
-        className="border border-primary py-2 px-4 font-semibold text-sm rounded-2xl cursor-pointer"
-      >
-        Count the number
-      </button>
+    <div className="flex  justify-center items-center min-h-screen gap-4">
+      <Counterfunction />
+      <CounterClass />
     </div>
   );
 }
